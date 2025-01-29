8c390cf21bdbcac413c1264f177d5bc20bfc1a3f
cylewaitforit <cyle@rightclickcode.com>
2025-01-29 07:45:39 -0600

    fix: run prettier

--- recommended.js
+++ recommended.js
@@ -1,12 +1,11 @@
 /** @type {import('stylelint').Config} */
 
 export default {
+  plugin: ['.'],
 
-	plugin: ['.'],
-
-	rules: {
+  rules: {
     'a11y/media-prefers-reduced-motion': true,
     'a11y/no-outline-none': true,
     'a11y/selector-pseudo-class-focus': true,
-	},
-}
+  },
+};
